fda9f4fd8d82996ee755fa07c359134bdec8ae3a
Luca <lrazov7@gmail.com>
2020-01-25 15:49:09 +0100

    Initial commit

---
+++ fizzbuzz.py
@@ -0,0 +1,14 @@
+print("Hy! This is the FizzBuzz game.")
+
+end = input("Select a number between 1 and 100: ")
+end = int(end)  # convert string into number
+
+for num in range(1, end+1):
+    if num % 3 == 0:
+        print("fizz")
+    elif num % 5 == 0:
+        print("buzz")
+    elif num % 3 == 0 and num % 5 == 0:
+        print("fizzbuzz")
+    else:
+        print(num)
